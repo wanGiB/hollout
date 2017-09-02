42bef460fafa95a6913c989ebb5bc37450efae01
Code Flippa <codeflippa@gmail.com>
2017-09-02 09:46:07 -0700

    Bump parse version from 15.8 to 16.0

--- app/src/main/java/com/wan/hollout/utils/AppKeys.java
+++ app/src/main/java/com/wan/hollout/utils/AppKeys.java
@@ -5,10 +5,10 @@ package com.wan.hollout.utils;
  */
 public class AppKeys {
 
-    public static String APPLICATION_ID = "eA4Vmd61H4X7Mf1ftf0dAXW3iPIQMzESbQUXJqQU";
-    public static String SERVER_ENDPOINT = "https://pg-app-2mm1lfh6rauu8weycfs9176v84qhku.scalabl.cloud/1/";
-    public static String SERVER_CLIENT_KEY = "kELHXRtueYyY0tAoqqfLkMrPXzQzJTkUMPCqQUNQ";
-    public static final String CLOBBIT_FILES_BUCKET = "gs://clobbit-99d1e.appspot.com";
+    public static String APPLICATION_ID = "rcTdlMZXmTx1k83UANZttbyPRDNUNUSKPPJLT6LR";
+    public static String SERVER_ENDPOINT = "https://pg-app-lpmjel6u5j6uhjy9yzqielveeej79e.scalabl.cloud/1/";
+    public static String SERVER_CLIENT_KEY = "gxqNVW0hBlqEva7SNqIp61C6NNLX63EsUPTCTPKU";
+    public static final String CLOBBIT_FILES_BUCKET = "gs://hollout-825b4.appspot.com";
 
     static String BASE_URL = "https://www.googleapis.com/blogger/v3/blogs";
     public static String GOOGLE_API_KEY = "AIzaSyBgj4cHfIu6eSPLezCK6K4a9ba5w1kwWZQ";
