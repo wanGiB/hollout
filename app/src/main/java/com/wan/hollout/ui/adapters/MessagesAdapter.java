5e687e2ee5469f1f6feefda7e321ebd2ac130f64
Code Flipper <codeflippa@gmail.com>
2017-09-23 00:06:10 +0100

    Change recycler view scrollToPosition to recyclerview layout manger with offset support

--- app/src/main/java/com/wan/hollout/ui/adapters/MessagesAdapter.java
+++ app/src/main/java/com/wan/hollout/ui/adapters/MessagesAdapter.java
@@ -1,17 +1,12 @@
 package com.wan.hollout.ui.adapters;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.drawable.TransitionDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 import com.hyphenate.chat.EMMessage;
@@ -64,20 +59,11 @@ public class MessagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     private static final int INCOMING_MESSAGE_WITH_REACTION = 14;
     private static final int INCOMING_MESSAGE_WITH_GIF = 15;
 
-    private static Animation bounceAnimation;
-
-    private void initBounceAnimation(Context context) {
-        bounceAnimation = AnimationUtils.loadAnimation(context, R.anim.bounce);
-        bounceAnimation.setDuration(500);
-        bounceAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-    }
-
     public MessagesAdapter(Activity context, List<EMMessage> messages) {
         this.context = context;
         this.messages = messages;
         this.layoutInflater = LayoutInflater.from(context);
         this.calendar = Calendar.getInstance();
-        initBounceAnimation(context);
     }
 
     @Override
@@ -250,7 +236,6 @@ public class MessagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                 TransitionDrawable transition = (TransitionDrawable) itemView.getBackground();
                 transition.setCrossFadeEnabled(true);
                 transition.startTransition(3000);
-//                itemView.startAnimation(bounceAnimation);
                 AppConstants.bounceablePositions.clear();
             }
         }
